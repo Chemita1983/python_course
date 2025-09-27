5dc87f0984f75bb19b5b952f8c0564c537282c37
Chemita1983 <josemjimenezvega@gmail.com>
2025-09-27 17:25:49 +0200

    asignacion valores

---
+++ 01_elementos_basicos/02_asignacion_valores.py
@@ -0,0 +1,45 @@
+'''
+
+Python permite el tipado dinámico, donde una misma variable puede contener diferentes tipos de datos
+a lo largo del programa
+
+'''
+
+numero = 10
+print(numero)
+print(type(numero))
+
+numero = 10.56
+print(numero)
+print(type(numero))
+
+cadena = 'C'
+print(cadena)
+print(type(cadena))
+
+cadena = "Hola"
+print(cadena)
+print(type(cadena))
+
+cadena = "Hola, estoy 'estudiando'"
+print(cadena)
+print(type(cadena))
+
+valor = True
+print(valor)
+print(type(valor))
+
+numero1 = 5
+numero2 = 10.5
+suma = numero1 + numero2
+print(suma)
+print(type(suma))
+print("la suma es:", suma)
+
+# Tipado dinámico
+valor = 10
+print(valor)
+print(type(valor))
+valor = "Valor"
+print(valor)
+print(type(valor))
